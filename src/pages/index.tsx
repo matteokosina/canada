6b6105fa3fa50e07b1b92183d38d0eafb7805213
Matteo Kosina <matteokosina@dev.com>
2025-01-27 22:01:24 +0100

    feat: add texts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,8 @@ import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
 import Hero from "@site/src/components/Heading/header-hero";
+import ImageText from "@site/src/components/Texts/Text";
+import Message from "@site/src/components/Message/Message";
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -24,6 +26,18 @@ export default function Home(): ReactNode {
       description="Follow me on my semester abroad ...">
       <HomepageHeader />
       <main>
+          <ImageText
+              left={true}
+              title="Canada"
+              text="Canada, the second-largest country in the world, is known for its breathtaking natural landscapes and cultural diversity."
+              imageUrl="./img/canada-patch.png"
+          />
+          <ImageText
+              left={false}
+              title="Vancouver"
+              text="Vancouver is the largest city in British Columbia, it is a vibrant city known for its stunning landscapes and multicultural population."
+              imageUrl="./img/people-patch.png"
+          />
 
       </main>
     </Layout>

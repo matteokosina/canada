3b812105284575bec17a7d1d02083dd28e6ee68c
Matteo Kosina <matteokosina@dev.com>
2025-02-12 20:25:07 +0100

    fix: adjust gh editURL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -50,7 +50,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/matteokosina/canada',
+            'https://github.com/matteokosina/canada/blob/main',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',

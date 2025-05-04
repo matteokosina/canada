788fdf5b7f6e6032daf552b10986cbe769a3d673
Matteo Kosina <matteokosina@dev.com>
2025-05-04 13:20:35 +0200

    chore: codestyle

--- src/components/ui/background-gradient-animation.tsx
+++ src/components/ui/background-gradient-animation.tsx
@@ -3,21 +3,21 @@ import { cn } from "../../lib/lib";
 import { useEffect, useRef, useState } from "react";
 
 export const BackgroundGradientAnimation = ({
-                                              gradientBackgroundStart = "rgb(108, 0, 162)",
-                                              gradientBackgroundEnd = "rgb(0, 17, 82)",
-                                              firstColor = "18, 113, 255",
-                                              secondColor = "221, 74, 255",
-                                              thirdColor = "100, 220, 255",
-                                              fourthColor = "200, 50, 50",
-                                              fifthColor = "180, 180, 50",
-                                              pointerColor = "140, 100, 255",
-                                              size = "80%",
-                                              blendingValue = "hard-light",
-                                              children,
-                                              className,
-                                              interactive = true,
-                                              containerClassName,
-                                            }: {
+  gradientBackgroundStart = "rgb(108, 0, 162)",
+  gradientBackgroundEnd = "rgb(0, 17, 82)",
+  firstColor = "18, 113, 255",
+  secondColor = "221, 74, 255",
+  thirdColor = "100, 220, 255",
+  fourthColor = "200, 50, 50",
+  fifthColor = "180, 180, 50",
+  pointerColor = "140, 100, 255",
+  size = "80%",
+  blendingValue = "hard-light",
+  children,
+  className,
+  interactive = true,
+  containerClassName,
+}: {
   gradientBackgroundStart?: string;
   gradientBackgroundEnd?: string;
   firstColor?: string;

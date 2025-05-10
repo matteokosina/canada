d3888757936d7acedffd79274d1bffc9ce713fd4
matteokosina <matteokosina@icloud.com>
2025-05-10 12:35:46 +0200

    fix: bg color for darkmode

--- src/components/RecentPost/RecentPost.tsx
+++ src/components/RecentPost/RecentPost.tsx
@@ -10,13 +10,13 @@ import { recentPost } from "./recent-post";
 import ReactMarkdown from "react-markdown";
 export default function RecentPost() {
   return (
-    <div className="flex flex-col items-center justify-center w-full py-10">
+    <div className="flex flex-col items-center justify-center w-full py-10" >
         <h1 className="text-3xl font-bold text-center text-black dark:text-white">
            Aktueller Blog-Post
         </h1>   
       <a
         href={recentPost.link}
-        className="block p-4 bg-white rounded-lg transition duration-300 mx-auto"
+        className="block p-4 rounded-lg transition duration-300 mx-auto"
       >
         <Card className="max-w-3xl mx-auto">
           <CardHeader className="flex flex-row justify-between items-start">

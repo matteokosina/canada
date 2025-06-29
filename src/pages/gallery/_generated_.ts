2af3c54d6e6b8f29c899469b74a2d8d7affefc2e
kosinamatteo <matteokosina@icloud.com>
2025-06-29 02:55:36 +0200

    Update _generated_.ts

--- src/pages/gallery/_generated_.ts
+++ src/pages/gallery/_generated_.ts
@@ -1,3 +1,34 @@
 // Auto-generated media list
 
-export const mediaList = [];
+export const mediaList = [
+  {
+    "url": "../media/IMG_4102.jpeg",
+    "type": "image",
+    "width": 4284,
+    "height": 5712
+  },
+  {
+    "url": "../media/IMG_4104.jpeg",
+    "type": "image",
+    "width": 4032,
+    "height": 3024
+  },
+  {
+    "url": "../media/IMG_4108.jpeg",
+    "type": "image",
+    "width": 5712,
+    "height": 4284
+  },
+  {
+    "url": "../media/IMG_4118.jpeg",
+    "type": "image",
+    "width": 4284,
+    "height": 5712
+  },
+  {
+    "url": "../media/IMG_4127.jpg",
+    "type": "image",
+    "width": 4284,
+    "height": 5712
+  }
+];

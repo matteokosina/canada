c8b4067cf630e608e5d5077fbc8ab405d77b6bc1
kosinamatteo <matteokosina@icloud.com>
2025-06-29 22:25:45 -0700

    update erster tag post

--- src/components/RecentPost/recent-post.ts
+++ src/components/RecentPost/recent-post.ts
@@ -4,7 +4,7 @@ export const recentPost = {
     author: "Matteo Kosina",
     authorAvatar: "https://avatars.githubusercontent.com/u/74454734?v=4",
     link: "https://matteokosina.github.io/canada/blog/der_erste_tag",
-    filename: "2025-06-30-der_erste_tag.md",
+    filename: "2025-06-30-der_erste_tag.mdx",
     date: "30. Juni 2025",
     content: 'Unser Tag begann mit einem Spaziergang über einen Farmers-Market 🛍️. Dort konnten wir dem Angebot von 2 kg Blaubeeren 🫐 nicht widerstehen und mussten sie einfach mitnehmen. Danach machten wir uns auf den Weg ins City Center, wo wir die berühmte Gastown Steam Clock ⏰ in Vancouver bewunderten.Später besuchten wir den Hafen, an dem zahlreiche Wasserflugzeuge ✈️ starteten und landeten. Nicht weit entfernt ragten die beeindruckenden Wolkenkratzer 🏙️ in den Himmel. Nachdem wir uns durch die Straßen Vancouvers treiben ließen, machten wir uns auf den Heimweg.Den Abend ließen wir mit einer Joggingrunde 🏃‍♂️ ausklingen. Die Landschaft, die von oben so flach aussah, entpuppte sich dabei als überraschend hügelig 🏞️ – eine perfekte Gelegenheit, uns noch einmal richtig auszupowern.Bis bald,  Matteo'
 };

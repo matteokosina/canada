d7cd40007ba3a74d00c1adc29bffef978db8e160
kosinamatteo <matteokosina@icloud.com>
2025-06-29 18:54:49 -0700

    update generated files

--- src/pages/gallery/_generated_.ts
+++ src/pages/gallery/_generated_.ts
@@ -2,27 +2,33 @@
 
 export const mediaList = [
   {
-    url: "https://matteokosina.github.io/canada/media/FRA-KEF.jpeg",
-    type: "image",
-    width: 4032,
-    height: 3024,
+    "url": "../media/FRA-KEF.jpeg",
+    "type": "image",
+    "width": 4032,
+    "height": 3024
   },
   {
-    url: "https://matteokosina.github.io/canada/media/Flug.jpeg",
-    type: "image",
-    width: 4257,
-    height: 4290,
+    "url": "../media/Flug.jpeg",
+    "type": "image",
+    "width": 4257,
+    "height": 4290
   },
   {
-    url: "https://matteokosina.github.io/canada/media/KEV-YVR.jpeg",
-    type: "image",
-    width: 5712,
-    height: 4284,
+    "url": "../media/KEV-YVR.jpeg",
+    "type": "image",
+    "width": 5712,
+    "height": 4284
   },
   {
-    url: "https://matteokosina.github.io/canada/media/Unterkunft.jpg",
-    type: "image",
-    width: 4257,
-    height: 4059,
+    "url": "../media/Unterkunft.jpg",
+    "type": "image",
+    "width": 4257,
+    "height": 4059
   },
+  {
+    "url": "../media/aufzug.jpg",
+    "type": "image",
+    "width": 3449,
+    "height": 3950
+  }
 ];

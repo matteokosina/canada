f30ff837a5aaae73afecd0e32e9b317c837c1d9c
matteokosina <matteokosina@icloud.com>
2025-10-26 16:09:02 +0100

    fix: allow sidebar to show all posts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,6 +52,7 @@ const config: Config = {
         },
         blog: {
           showReadingTime: true,
+          blogSidebarCount: "ALL",
           feedOptions: {
             type: ["rss", "atom"],
             xslt: true,
